a795d064cfde1cadb9f52c687639a2a8edc71382
Didenko Victoria <155799784+misfits3z@users.noreply.github.com>
2025-04-30 17:17:15 +0800

    task_1 done

--- task_2.py
+++ task_2.py
@@ -7,19 +7,20 @@ class LongestCommonWord(Trie):
         if not isinstance(strings, list) or not all(
             isinstance(s, str) for s in strings
         ):
-            raise ValueError("Вхідні дані мають бути списком рядків.")
+            raise ValueError("Input data must be a list of strings.")
         if not strings:
             return ""
 
-        # Побудова Trie з усіх слів
+        # Побудова дерева з усіх слів
         for word in strings:
             self.put(word)
 
         # Знаходження найдовшого спільного префікса
         prefix = ""
         node = self.root
+
         while True:
-            if len(node.children) != 1 or node.is_end:
+            if len(node.children) != 1:  
                 break
             char, next_node = next(iter(node.children.items()))
             prefix += char
@@ -32,11 +33,16 @@ if __name__ == "__main__":
     trie = LongestCommonWord()
     strings = ["flower", "flow", "flight"]
     assert trie.find_longest_common_word(strings) == "fl"
+    # print(trie.find_longest_common_word(strings))  
 
     trie = LongestCommonWord()
     strings = ["interspecies", "interstellar", "interstate"]
     assert trie.find_longest_common_word(strings) == "inters"
+    # print(trie.find_longest_common_word(strings))  
 
     trie = LongestCommonWord()
     strings = ["dog", "racecar", "car"]
     assert trie.find_longest_common_word(strings) == ""
+    # print(trie.find_longest_common_word(strings))  
+
+   
